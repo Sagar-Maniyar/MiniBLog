d0804be5c5c298f8b655e4ef3d372e1872bbaa47
Sagar-Maniyar <131576017+Sagar-Maniyar@users.noreply.github.com>
2023-10-17 15:24:22 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/5.0.7/sweetalert2.min.css">
-    
-    
-    
+
     <style>
         .alert-success.alertcustom{
             display: flex;
